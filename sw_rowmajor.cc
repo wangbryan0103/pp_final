be7d8a5bb78151a2da1cd966b28a936594847e69
edward <edwardkuo0909@gmail.com>
2024-11-29 16:08:17 +0800

    row major finish

--- sw_rowmajor.cc
+++ sw_rowmajor.cc
@@ -20,15 +20,17 @@ using namespace std;
 #define mismatch_score -1
 
 int length = 10000;
-//string seqA,seqB;
-string seqA ="GATAGTATTACTAGTACGTTATTTGCCTGCTGC", seqB ="GATCTCGTCACTACTAATCGTACGTCATGCTGCT";
+
+/*string seqA ="GATAGTATTACTAGTACGTTATTTGCCTGCTGC", seqB ="GATCTCGTCACTACTAATCGTACGTCATGCTGCT";
 int m = seqA.length();
 int n = seqB.length();
-vector<std::vector<int>> score(m + 1, std::vector<int>(n + 1, 0));
+vector<std::vector<int>> score(m + 1, std::vector<int>(n + 1, 0));*/
 int max_score_parallel = 0, max_i_parallel = 0, max_j_parallel = 0;
-/*vector<vector<int>> score(length + 1, vector<int>(length + 1, 0));
-sem_t* sems;*/
+string seqA,seqB;
+vector<vector<int>> score(length + 1, vector<int>(length + 1, 0));
+//sem_t* sems;
 
+int thread_count;
 
 static inline void calculate(const string &seqA, const string &seqB, vector<vector<int>> &score, int &max_score, int &max_i, int &max_j, int i, int j){
 
@@ -56,7 +58,7 @@ static inline void calculate(const string &seqA, const string &seqB, vector<vect
 }
 
 void SmithWaterman_serial(const string &seqA, const string &seqB, vector<vector<int>> &score, int &max_score, int &max_i, int &max_j) {
-    
+    auto start_ = chrono::high_resolution_clock::now();
     int rows = seqA.size() + 1; 
     int cols = seqB.size() + 1;
     
@@ -65,30 +67,55 @@ void SmithWaterman_serial(const string &seqA, const string &seqB, vector<vector<
             calculate(seqA, seqB, score, max_score, max_i, max_j, i, j);
         }
     }
+    auto end_ = chrono::high_resolution_clock::now();
+    auto minThread = chrono::duration_cast<chrono::nanoseconds>(end_ - start_);
+    cout<<minThread.count()<<endl;
 }
 
-struct ThreadArgs {
-    int max_i;
-    int max_j;
-    int thread_id;
-    int max_score;
-};
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+int current_stage = 0; 
 void* threadcal(void* rank){
-    
+    auto start_ = chrono::high_resolution_clock::now();
     int rows = seqA.size() + 1; 
     int cols = seqB.size() + 1;
-    //ThreadArgs* targs = (ThreadArgs*)args;
+
     long my_rank = (long) rank;
     
-    int my_s = rows/1 * my_rank;
-    int my_e = rows/1 * (my_rank+1);
-    cout<<my_rank<<" ";
-    for(int i = my_s+1; i < my_e; ++i) {
-        for (int j = 1; j < cols; ++j) {
-            calculate(seqA, seqB, score, max_score_parallel, max_i_parallel, max_j_parallel, i, j);
+    int my_s = rows/thread_count * my_rank;
+    int my_e = rows/thread_count * (my_rank+1);
+    //use row cut
+    /*if (my_rank==0){
+        
+        for (int i = my_s+1; i < (my_e+1); ++i) {
+            for (int j = 1; j < cols; ++j) {
+                calculate(seqA, seqB, score, max_score_parallel, max_i_parallel, max_j_parallel, i, j);
+            }
         }
-    }
-    /*if (my_rank == 0){
+        pthread_mutex_lock(&mut);
+        current_stage++;
+        pthread_cond_broadcast(&cond); 
+        pthread_mutex_unlock(&mut);
+    }else{
+        pthread_mutex_lock(&mut);
+        while (current_stage != my_rank)  {
+            pthread_cond_wait(&cond, &mut);
+        }
+        pthread_mutex_unlock(&mut);
+        for (int i = my_s; i < (my_e); ++i) {
+            for (int j = 1; j < cols; ++j) {
+                calculate(seqA, seqB, score, max_score_parallel, max_i_parallel, max_j_parallel, i, j);
+            }
+        }
+        pthread_mutex_lock(&mut);
+        current_stage++;
+        pthread_cond_broadcast(&cond);
+        pthread_mutex_unlock(&mut);
+    }*/
+
+
+    // two threads
+    if (my_rank == 0){
         for (int i = 1; i < rows; ++i) {
             calculate(seqA, seqB, score, max_score_parallel, max_i_parallel, max_j_parallel, i, 1);
         }
@@ -98,53 +125,36 @@ void* threadcal(void* rank){
         for (int j = 2; j < cols/2; ++j) {
             calculate(seqA, seqB, score, max_score_parallel,max_i_parallel, max_j_parallel, 2, j);
         }
-        for(int i = 1; i < rows/2; ++i) {
-            for (int j = 1; j < cols; ++j) {
+        for(int i = 3; i < rows; ++i) {
+            for (int j = 2; j < cols/2; ++j) {
                 calculate(seqA, seqB, score, max_score_parallel, max_i_parallel, max_j_parallel, i, j);
+                if(j == cols/2 -1){
+                    pthread_mutex_lock(&mut);
+                    current_stage = i;
+                    pthread_cond_broadcast(&cond);
+                    pthread_mutex_unlock(&mut);
+                }
             }
         }
-    }else if (my_rank == 1){
-        for(int i = rows/2; i < rows; ++i) {
-            for (int j = 1; j < cols; ++j) {
-                calculate(seqA, seqB, score,max_score_parallel, max_i_parallel, max_j_parallel, i, j);
-            }
-        }
-    }*/
-    /*else {
+    }else{
         for(int i = 2; i < rows; ++i) {
-            for (int j = cols/2+1; j < cols; ++j) {
+            pthread_mutex_lock(&mut1);
+            if (i != rows-1){
+                while (current_stage < i+1){
+                    pthread_cond_wait(&cond, &mut1);
+                }
+            }
+            pthread_mutex_unlock(&mut1);
+            
+            for (int j = cols/2-1; j < cols; ++j) {
                 calculate(seqA, seqB, score,max_score_parallel, max_i_parallel, max_j_parallel, i, j);
             }
         }
-    }*/
-    
+    }
+    auto end_ = chrono::high_resolution_clock::now();
+    auto minThread = chrono::duration_cast<chrono::nanoseconds>(end_ - start_);
+    cout<<minThread.count()<<endl;
     return NULL;
-}
-
-int thread_count;
-void rowmajor(const int thread_count) {
-    int thread;
-    pthread_t* thread_handles;
-    
-    //sems = (sem_t*) malloc(thread_count*sizeof(sem_t));
-    thread_handles = (pthread_t*) malloc (thread_count*sizeof(pthread_t));
-    /*sem_init(&sems[0], 0, 1);
-    for (thread = 1; thread < thread_count; thread++)
-        sem_init(&sems[thread], 0, 0);*/
-    for (thread = 0; thread < thread_count; thread++){
-        auto start_ = chrono::high_resolution_clock::now();
-        pthread_create(&thread_handles[thread], NULL,threadcal, (void*)thread); 
-        auto end_ = chrono::high_resolution_clock::now();
-        auto minThread = chrono::duration_cast<chrono::nanoseconds>(end_ - start_);
-        cout<<minThread.count()<<endl;
-    }
-    for (thread = 0; thread < thread_count; thread++){
-        pthread_join(thread_handles[thread], NULL);
-        //sem_destroy(&sems[thread]);
-    }
-
-    free(thread_handles);
-    //free(sems);
 }
 bool ans_accuracy(int &score_s, int &score_w, int &i_s, int &i_w, int &j_s, int j_w)
 {
@@ -180,12 +190,31 @@ void generate_similar_seq(const string &seqA, string &seqB, int length, double s
 int main(int argc, char **argv) {
     double similarity = 0.5;
 
-    /*generate_random_seq(seqA, length);
-    generate_similar_seq(seqA, seqB, length, similarity);*/
-
+    generate_random_seq(seqA, length);
+    generate_similar_seq(seqA, seqB, length, similarity);
     thread_count = strtol(argv[1], NULL, 10);
     auto strat_rowmajor = chrono::high_resolution_clock::now();
-    rowmajor(thread_count);
+
+    int thread;
+    pthread_t* thread_handles;
+    
+    //sems = (sem_t*) malloc(thread_count*sizeof(sem_t));
+    thread_handles = (pthread_t*) malloc (thread_count*sizeof(pthread_t));
+    /*sem_init(&sems[0], 0, 1);
+    for (thread = 1; thread < thread_count; thread++)
+        sem_init(&sems[thread], 0, 0);*/
+    for (thread = 0; thread < thread_count; thread++){
+        
+        pthread_create(&thread_handles[thread], NULL,threadcal, (void*)thread); 
+        
+    }
+    for (thread = 0; thread < thread_count; thread++){
+        pthread_join(thread_handles[thread], NULL);
+        //sem_destroy(&sems[thread]);
+    }
+    pthread_mutex_destroy(&mut);
+    free(thread_handles);
+    //free(sems);
     auto end_rowmajor = chrono::high_resolution_clock::now();
     auto duration_rowmajor = chrono::duration_cast<chrono::nanoseconds>(end_rowmajor - strat_rowmajor);
 
@@ -197,7 +226,7 @@ int main(int argc, char **argv) {
     auto end_serial = chrono::high_resolution_clock::now();
     auto duration_serial = chrono::duration_cast<chrono::nanoseconds>(end_serial - start_serial);
 
-    cout<<"   ";
+    /*cout<<"   ";
     for(int k=0;k<seqB.length();k++)
         cout<<seqB[k]<<"  ";
 	    cout<<endl;
@@ -207,7 +236,7 @@ int main(int argc, char **argv) {
 			        cout<<score[i][j]<<"  ";
 		}
 		cout<<endl;
-	}
+	}*/
 
     // cout << "=====sequence A=====" << endl << seqA << endl;
     // cout << "====================" << endl << endl;
