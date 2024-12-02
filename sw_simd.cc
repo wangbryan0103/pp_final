b946cf817c419cb31658d3ba1f881f9c39cabfa0
wangbryan0103 <wang.bryan0103@gmail.com>
2024-12-02 15:36:01 +0800

    simd not finish

--- sw_simd.cc
+++ sw_simd.cc
@@ -8,14 +8,16 @@
 #include <algorithm> 
 #include <chrono>
 #include <utility>
+#include <immintrin.h>
 
 #define BEFORE_MAX 0
-#define MAX_DIAG 1
+#define JUST_AFTER 1
 #define ATFER_MAX 2
+
 using namespace std;
 
-int gap_score = -7;
-int match_score = 5;
+int GAP_SCORE = -7;
+int MATCH_SCORE = 5;
 
 
 
@@ -55,16 +57,22 @@ bool ans_accuracy(int &score_s, int &score_w, int &i_s, int &i_w, int &j_s, int 
 
 
 
-void SmithWaterman_serial(vector<vector<int>>& score,const string seq1,const string seq2, int &max_score_serial, int &max_i_serial, int &max_j_serial){
+void SmithWaterman_serial(vector<vector<int>>& score,const string seqA,const string seqB, int &max_score_serial, int &max_i_serial, int &max_j_serial){
 	int mismatch_score;
     for(int i=1; i<score.size();i++){
 		for (int j=1;j<score[i].size();j++){
-			 if (seq1[i-1]==seq2[j-1]){
-				score[i][j]=score[i-1][j-1] + match_score;
+			 if (seqA[i-1]==seqB[j-1]){
+				score[i][j]=score[i-1][j-1] + MATCH_SCORE;
 			}else {
-				if(seq1[i-1]=='A'&&seq2[j-1]=='G'||seq1[i-1]=='G'&&seq2[j-1]=='A'||seq1[i-1]=='C'&&seq2[j-1]=='T'||seq1[i-1]=='T'&&seq2[j-1]=='C') mismatch_score=-1;
-				else mismatch_score=-3;
-				score[i][j] = max({score[i][j-1]-gap_score,score[i-1][j]-gap_score,score[i-1][j-1]+mismatch_score,0});
+				if ((seqA[i - 1] == 'A' && seqB[j - 1] == 'G') || (seqA[i - 1] == 'G' && seqB[j - 1] == 'A') ||
+                    (seqA[i - 1] == 'C' && seqB[j - 1] == 'T') || (seqA[i - 1] == 'T' && seqB[j - 1] == 'C')) {
+                        mismatch_score = -1;
+                }
+                else
+                {
+                    mismatch_score = -3;
+                }
+				score[i][j] = max({score[i][j-1]-GAP_SCORE,score[i-1][j]-GAP_SCORE,score[i-1][j-1]+mismatch_score,0});
 			}
 			if (score[i][j]>max_score_serial){
 				max_i_serial=i;
@@ -76,51 +84,13 @@ void SmithWaterman_serial(vector<vector<int>>& score,const string seq1,const str
 }
 
 
-int calculate_smid(const string &seqA, const string &seqB,
-                   vector<int> &prev1, vector<int> &prev2, vector<int> curr, 
-                   int len_diag, int d, int state){
 
 
-    const __m256i gap_vec = _mm256_set1_epi32(gap_score);
-    const __m256i match_vec = _mm256_set1_epi32(match_score);
-    const __m256i mismatch1_vec = _mm256_set1_epi32(-1);
-    const __m256i mismatch3_vec = _mm256_set1_epi32(-3);
-
-    __m256i curr_vec = _mm256_stezero_si256();
-    __256i prev1_vec = _mm256_loadu_si256((__256*)&prev1[d]);
-    __256i prev2_vec = _mm256_loadu_si256((__256*)&prev2[d]);
-
-
-    // int mismatch_score;
-    // int curr;
-
-    // if (seqA[i - 1] == seqB[j - 1])
-    // {
-    //     int index = (state == BEFORE_MAX)? (d-1) : (state == MAX_DIAG)? d : (d+1);
-    //     curr = prev1[index] + match_score;
-    // }else
-    // {
-    //     if ((seqA[i - 1] == 'A' && seqB[j - 1] == 'G') || (seqA[i - 1] == 'G' && seqB[j - 1] == 'A') ||
-    //         (seqA[i - 1] == 'C' && seqB[j - 1] == 'T') || (seqA[i - 1] == 'T' && seqB[j - 1] == 'C')) {
-    //         mismatch_score = -1;
-    //     }else
-    //     {
-    //         mismatch_score = -3;
-    //     }
-    //     if(state == BEFORE_MAX){
-    //         curr = max(prev2[d-1] + gap_score, max(prev2[ d ] + gap_score, max(prev1[d-1] + mismatch_score, 0)));
-    //     }else if (state == MAX_DIAG){
-    //         curr = max(prev2[ d ] + gap_score, max(prev2[d-1] + gap_score, max(prev1[ d ] + mismatch_score, 0)));
-    //     }else{
-    //         curr = max(prev2[ d ] + gap_score, max(prev2[d+1] + gap_score, max(prev1[d+1] + mismatch_score, 0)));
-    //     }
-    // }
-    // return curr;
-}
-
-
-//assune len_A > len_B
 void SmithWaterman_parallel(const string &seqA, const string &seqB, int &max_score, int &max_i, int &max_j) {
+    const int MATCH_SCORE = 2;
+    const int GAP_SCORE = -2;
+    const int MISMATCH_SCORE = -3;
+
     int rows = seqA.size() + 1;
     int cols = seqB.size() + 1;
 
@@ -128,53 +98,91 @@ void SmithWaterman_parallel(const string &seqA, const string &seqB, int &max_sco
     vector<int> prev2(rows, 0);
     vector<int> curr(rows, 0);
 
-    #pragma omp parallel
-    {        
-        int local_max_score = 0, local_i = 0, local_j = 0;
+    for (int diag = 2; diag < rows + cols - 1; ++diag) {
+        int start_row = max(0, diag - (rows - 1));
+        int end_row = min(diag, rows - 1);
+        int len_diag = end_row - start_row + 1;
 
-        for (int diag = 2; diag < rows + cols - 1; ++diag)
-        {
-            int start_row = max(0, diag - (rows-1)); 
-            int end_row = min(diag, rows - 1);
-            int len_diag = end_row - start_row + 1;
+        int state = (start_row == 0) ? BEFORE_MAX : (start_row == 1) ? MAX_DIAG : AFTER_MAX;
 
-            int state = (start_row == 0)? 0 : (start_row == 1)? 1 : 2;
+
+        __m256i vec_match_score    = _mm256_set1_epi8(MATCH_SCORE);
+        __m256i vec_mismatch_score = _mm256_set1_epi8(MISMATCH_SCORE);
+        __m256i vec_gap_score      = _mm256_set1_epi8(GAP_SCORE);
+
+        int d = 0;
+        for (; d + 8 <= len_diag; d += 8) {
             
-            #pragma omp for
-            for(int d = 0; d < len_diag; d += 8)
-            {
-                calculate_smid(seqA, seqB, prev1, prev2, curr, len_diag, d, state);
-                // int i = start_row + d;
-                // int j = diag - i;
-                // if(i == 0 || j == 0) curr[d] = 0;
-                // else curr[d] = calculate_parallel(seqA, seqB, prev1, prev2, i, j, d, state);
+            __m256i vec_seqA = _mm256_loadu_si256((__m256i *)(seqA.data() + start_row + d - 1));
+            __m256i vec_seqB = _mm256_loadu_si256((__m256i *)(seqB.data() + diag - (start_row + d) - 1));
 
-                // if(curr[d] > local_max_score)
-                // {
-                //     local_max_score = curr[d];
-                //     local_i = i;
-                //     local_j = j;
-                // }
+            __m256i vec_match    = _mm256_cmpeq_epi8(vec_seqA, vec_seqB);
+            __m256i vec_mismatch = _mm256_set1_epi8(1);
+            
+            __m256i vec_score = _mm256_blendv_epi8(vec_mismatch_score, vec_match_score, vec_match);
+            
+            __m256i vec_prev1, vec_prev2;
+            if (state == BEFORE_MAX) {
+                vec_prev1 = _mm256_loadu_si256((__m256i *)(prev1.data() + d - 1));
+                vec_prev2 = _mm256_max_epi32(_mm256_loadu_si256((__m256i *)(prev2.data() + d - 1)),
+                                             _mm256_loadu_si256((__m256i *)(prev2.data() +   d  )));
+
+            } else if (state == MAX_DIAG) {
+                vec_prev1 = _mm256_loadu_si256((__m256i *)(prev1.data() + d));
+                vec_prev2 = _mm256_max_epi32(_mm256_loadu_si256((__m256i *)(prev2.data() + d - 1)),
+                                             _mm256_loadu_si256((__m256i *)(prev2.data() +   d  )));
+
+            } else { 
+                vec_prev1 = _mm256_loadu_si256((__m256i *)(prev1.data() + d + 1));
+                vec_prev2 = _mm256_max_epi32(_mm256_loadu_si256((__m256i *)(prev2.data() + d + 1)),
+                                             _mm256_loadu_si256((__m256i *)(prev2.data() +   d  )));
             }
-            #pragma omp barrier 
-            
-            #pragma omp single
-            {
 
-                prev1.swap(prev2);
-                prev2.swap(curr); 
-                fill(curr.begin(), curr.end(), 0);
+            
+            
+            __m256i vec_curr = _mm256_max_epi8(_mm256_add_epi8(vec_prev1, vec_score), vec_gap_score);
+
+            
+            _mm256_storeu_si256((__m256i *)(curr.data() + d), vec_curr);
+
+            
+            for (int k = 0; k < 8; ++k) {
+                int score = curr[d + k];
+                if (score > max_score) {
+                    max_score = score;
+                    max_i = start_row + d + k;
+                    max_j = diag - max_i;
+                }
             }
         }
 
-        #pragma omp critical
-        {
-            if (local_max_score > max_score){
-                max_score = local_max_score;
-                max_i = local_i;
-                max_j = local_j;
+        
+        for (; d < len_diag; ++d) {
+            int i = start_row + d;
+            int j = diag - i;
+            if (i == 0 || j == 0) {
+                curr[d] = 0;
+            } else {
+                int score = (seqA[i - 1] == seqB[j - 1]) ? MATCH_SCORE : MISMATCH_SCORE;
+                if (state == BEFORE_MAX) {
+                    curr[d] = max(prev1[d - 1] + score, max(prev2[d - 1] + GAP_SCORE, 0));
+                } else if (state == MAX_DIAG) {
+                    curr[d] = max(prev1[d] + score, max(prev2[d] + GAP_SCORE, 0));
+                } else {
+                    curr[d] = max(prev1[d + 1] + score, max(prev2[d + 1] + GAP_SCORE, 0));
+                }
+            }
+
+            if (curr[d] > max_score) {
+                max_score = curr[d];
+                max_i = i;
+                max_j = j;
             }
         }
+
+        prev1.swap(prev2);
+        prev2.swap(curr);
+        fill(curr.begin(), curr.end(), 0);
     }
 }
 
@@ -188,11 +196,11 @@ int main(int argc, char **argv) {
     // ====================
     // generate sequence
     // ====================
-    string seqA;// = "GATCTCGT";
-    string seqB;// = "GATAGCAT";
+    string seqA; // = "GATCTCGT";
+    string seqB; // = "GATAGCAT";
 
 
-    int length = 20000;
+    int length = 50000;
     double similarity = 0.7;
     generate_random_seq(seqA, length);
     generate_similar_seq(seqA, seqB, length, similarity);
@@ -204,7 +212,7 @@ int main(int argc, char **argv) {
     int max_score_parallel = 0, max_i_parallel = 0, max_j_parallel = 0;
 
     auto start_parallel = chrono::high_resolution_clock::now();
-    SmithWaterman_parallel(seqA, seqB, max_score_parallel, max_i_parallel, max_j_parallel);
+    SmithWaterman_SIMD(seqA, seqB, max_score_parallel, max_i_parallel, max_j_parallel);
     auto end_parallel = chrono::high_resolution_clock::now();
     auto duration_parallel = chrono::duration_cast<chrono::nanoseconds>(end_parallel - start_parallel);
 
@@ -224,23 +232,24 @@ int main(int argc, char **argv) {
 
     
     bool score_test = ans_accuracy(max_score_serial, max_score_parallel, max_i_serial, max_i_parallel, max_j_serial, max_j_parallel);
-   
-
     if(score_test == true) cout << "| score_test passed |" << endl << endl;
     else cout << "| score_test failed |" << endl << endl;
 
+
     cout << "Serial Score   : " << max_score_serial << " at (" << max_i_serial << ", " << max_j_serial << ")" << endl;
     cout << "Parallel Score : " << max_score_parallel << " at (" << max_i_parallel << ", " << max_j_parallel << ")" << endl << endl;
+
 
     cout << "==========  serial  ==========" << endl;
     cout << "time: " << duration_serial.count() << " ns" << endl;
     cout << "========== parallel ==========" << endl;
     cout << "time: " << duration_parallel.count() << " ns" << endl << endl;
 
+
     cout << "speedup: " << (double)duration_serial.count() / (double)duration_parallel.count() << endl;
 
     return 0;
 }
 /*
-make; srun ./sw
+make; srun -c 1 ./sw
 */
